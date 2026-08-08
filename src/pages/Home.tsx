96e85d09343638fccd7d778c97af2f81114c3cce
Gantavya <gantavyamishra@gmail.com>
2026-08-08 23:03:25 +0530

    sandImg added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import bg from "../assets/086ee623dc5facfe1545894c42f50d8ec74859c9.jpg";
+    import bg from "../assets/086ee623dc5facfe1545894c42f50d8ec74859c9.jpg";
 import styles from "../styles/Home.module.scss"
-import sandImg from "../assets/sandImg.png"
-export default function Home() {
-    return (
+import sandImg from "../assets/sand.png"
+    export default function Home() {
+        return (
         <div>
             <div
             className="background"
@@ -12,5 +12,5 @@ export default function Home() {
             <div className={styles.sand}>
                 <img src={sandImg} className={styles.sandImg} />
             </div>
-        </div> )
-}
+            </div> )
+    }
