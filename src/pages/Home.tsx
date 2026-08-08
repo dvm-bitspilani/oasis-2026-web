d32810566dfcc312b80a04a1ed3e6e1be5558066
Gantavya <gantavyamishra@gmail.com>
2026-08-08 22:28:21 +0530

    Initial bg added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import bg from "../assets/086ee623dc5facfe1545894c42f50d8ec74859c9.jpg";
+
+export default function Home() {
+    return (
+        <div
+        className="background"
+         style={{background: `url(${bg}) no-repeat center center/cover`,height: "100svh", width: "100svw"}}
+         >
+        </div> )
+}
