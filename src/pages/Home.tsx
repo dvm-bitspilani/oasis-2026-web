af4d6e0b36b0bc0d6cbb899b354b177ea6258103
ujaasagarwal <ujaas1234@gmail.com>
2026-08-08 22:40:11 +0530

    sand added to the background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import bg from "../assets/086ee623dc5facfe1545894c42f50d8ec74859c9.jpg";
-
+import styles from "../styles/Home.module.scss"
+import sandImg from "../assets/sandImg.png"
 export default function Home() {
     return (
-        <div
-        className="background"
-         style={{background: `url(${bg}) no-repeat center center/cover`,height: "100svh", width: "100svw"}}
-         >
+        <div>
+            <div
+            className="background"
+            style={{background: `url(${bg}) no-repeat center center/cover`,height: "100svh", width: "100svw"}}
+            >
+            </div>
+            <div className={styles.sand}>
+                <img src={sandImg} className={styles.sandImg} />
+            </div>
         </div> )
 }
