94ea8a6e4dc97c6a8fdb8e5a58eb39381b4dfc9d
ujaasagarwal <ujaas1234@gmail.com>
2026-08-12 18:22:21 +0530

    preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Home from "./pages/Home";
-import Preloader from "./components/Preloader/Preloader";
+import Preloader from "./components/preloader/preloader";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
