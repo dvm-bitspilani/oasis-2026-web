5e0e6bfeff39954b971cd7e944307435f488512b
ujaasagarwal <ujaas1234@gmail.com>
2026-08-15 23:56:25 +0530

    branch

---
+++ src/pages/registration/components/Events/components/Category.module.scss
@@ -0,0 +1,25 @@
+// import styles from "./Reginput.module.scss";
+
+// interface ReginputProps {
+//   title: string;
+//   value?: string;
+// }
+
+// export default function Reginput({
+//   title,
+//   value = "",
+// }: ReginputProps) {
+//   return (
+//     <div className={styles.reginputContainer}>
+//       <h2 className={styles.inputTitle}>
+//         {title}
+//       </h2>
+
+//       <div className={styles.inputContainer}>
+//         <span className={styles.value}>
+//           {value}
+//         </span>
+//       </div>
+//     </div>
+//   );
+// }
