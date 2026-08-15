6419af658b2a1aa37640b9abc9bcd97930f99da7
ujaasagarwal <ujaas1234@gmail.com>
2026-08-15 23:50:03 +0530

    events

--- src/pages/registration/components/Events/Events.tsx
+++ src/pages/registration/components/Events/Events.tsx
@@ -598,9 +598,54 @@
 // });
 
 // export default Events;
-import styles from "./Events.module.scss"
-export default function EventsReg(){
-    return(
-      <h1 className={styles.hello}>EVENTS</h1>
-    )
+
+import styles from "./Events.module.scss";
+
+import RegBg from "../../../../assets/registration/reg/RegBg.png";
+import leftbottom from "../../../../assets/registration/reg/leftbottom.png";
+import lefttop from "../../../../assets/registration/reg/lefttop.png";
+import rightbottom from "../../../../assets/registration/reg/rightbottom.png";
+import righttop from "../../../../assets/registration/reg/righttop.png";
+import book from "../../../../assets/registration/reg/book.png";
+
+export default function EventsReg() {
+  return (
+    <div
+      className={styles.registerContainer}
+      style={{
+        backgroundImage: `url(${RegBg})`,
+      }}
+    >
+      <img
+        src={leftbottom}
+        className={styles.leftbottom}
+        alt="leftbottom"
+      />
+
+      <img
+        src={lefttop}
+        className={styles.lefttop}
+        alt="lefttop"
+      />
+
+      <img
+        src={rightbottom}
+        className={styles.rightbottom}
+        alt="rightbottom"
+      />
+
+      <img
+        src={righttop}
+        className={styles.righttop}
+        alt="righttop"
+      />
+
+      <div
+        className={styles.bookContainer}
+        style={{
+          backgroundImage: `url(${book})`,
+        }}
+      />
+    </div>
+  );
 }
