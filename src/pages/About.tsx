292dbe1c7ba83cb359c66b64fdcc08cd205e8980
apple <apple@ujaasagarwal.local>
2026-09-10 00:43:31 +0530

    aboutBBounce

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -209,7 +209,13 @@ const About = () => {
       }
     };
   }, [showVideo]);
-
+  gsap.to(scrollVidRef.current, {
+    y: 10,
+    duration: 0.5,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut",
+  });
   const togglePlay = useCallback(() => {
     const player = playerRef.current;
 
